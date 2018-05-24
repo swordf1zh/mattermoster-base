8a38954f9690ed28e98a3b685b8ae821613cc0ee
rtribaldos <rtribaldos@registro-publico.gob.pa>
2018-05-24 09:32:38 -0500

    Include Node.js HTTP server and Express based on express-generator

---
+++ app.js
@@ -0,0 +1,40 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+
+const app = express();
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+const indexRouter = require('./routes/index');
+app.use('/', indexRouter);
+
+/**
+ * ROUTE PLUGINS
+ * Require plugins here and set routes for them.
+ * Eg.:
+ * const mattermosterTodo = require('mattermoster-todo');
+ * app.use('/todo', mattermosterTodo);
+ */
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
